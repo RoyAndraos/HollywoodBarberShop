84a94df81e66503f8d16aa863ecccddbeb999816
Roy Andraos <roy_andraos@live.fr>
2024-06-06 07:46:57 -0400

    unused var

--- client/src/components/homepage/Barbers.js
+++ client/src/components/homepage/Barbers.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { LanguageContext } from "../contexts/LanguageContext";
