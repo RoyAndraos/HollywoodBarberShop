83687e9db826bdd10ca6ff45a64aa94c9d0a5559
Roy Andraos <roy_andraos@live.fr>
2025-01-15 14:44:45 -0500

    last check for overlap

--- server/server.js
+++ server/server.js
@@ -154,6 +154,84 @@ const getReservations = async (req, res) => {
 // POST ENDPOINTS
 // ---------------------------------------------------------------------------------------------
 
+const filterSlotBeforeFor2Duration = (slot) => {
+  const minuteToEdit = slot.slice(0, -2).split(":")[1];
+  if (minuteToEdit !== "00") {
+    const newMinute = parseInt(minuteToEdit) - 15;
+    if (newMinute === 0) {
+      return slot.split(":")[0] + ":00" + slot.slice(-2);
+    } else {
+      return (
+        slot.slice(0, -2).split(":")[0] +
+        ":" +
+        newMinute.toString() +
+        slot.slice(-2)
+      );
+    }
+  } else {
+    const newMinute = "45";
+    const hourToEdit = slot.slice(0, -2).split(":")[0];
+    if (hourToEdit !== "1" && hourToEdit !== "12") {
+      const newHour = parseInt(slot.slice(0, -2).split(":")[0]) - 1;
+      if (newHour.toString().length === 2) {
+        return newHour.toString() + ":" + newMinute + slot.slice(-2);
+      } else {
+        return "0" + newHour.toString() + ":" + newMinute + slot.slice(-2);
+      }
+    } else if (hourToEdit === "12") {
+      return "11:" + newMinute + "am";
+    } else {
+      const newHour = "12";
+      return newHour + ":" + newMinute + "pm";
+    }
+  }
+};
+
+const removeSlotsForOverLapping = (
+  serviceDuration,
+  todayReservationStartingSlots
+) => {
+  let slotsToRemove = [];
+  switch (serviceDuration) {
+    case "1":
+      break;
+    case "2":
+      todayReservationStartingSlots.forEach((slot) => {
+        const slotToEdit = filterSlotBeforeFor2Duration(slot);
+        slotsToRemove.push(slotToEdit);
+      });
+
+      break;
+    case "3":
+      todayReservationStartingSlots.forEach((slot) => {
+        const slotToEdit = filterSlotBeforeFor2Duration(slot);
+        slotsToRemove.push(slotToEdit);
+        const slotToEdit2 = filterSlotBeforeFor2Duration(slotToEdit);
+        slotsToRemove.push(slotToEdit2);
+      });
+      break;
+    case "4":
+      todayReservationStartingSlots.forEach((slot) => {
+        const slotToEdit = filterSlotBeforeFor2Duration(slot);
+        slotsToRemove.push(slotToEdit);
+        const slotToEdit2 = filterSlotBeforeFor2Duration(slotToEdit);
+        slotsToRemove.push(slotToEdit2);
+        const slotToEdit3 = filterSlotBeforeFor2Duration(slotToEdit2);
+        slotsToRemove.push(slotToEdit3);
+      });
+      break;
+    default:
+      break;
+  }
+
+  return slotsToRemove.map((slot) => {
+    if (slot[0] === "0") {
+      return slot.slice(1);
+    }
+    return slot;
+  });
+};
+
 const addReservation = async (req, res) => {
   const client = new MongoClient(MONGO_URI_RALF);
   const formData = req.body[0];
@@ -194,6 +272,42 @@ const addReservation = async (req, res) => {
     //   to: "Ty's Number",
     // });
 
+    //Last check for Overlapping reservations
+    //STEP ONE: Get all reservations for the selected date and barber
+    const reservations = await db
+      .collection("reservations")
+      .find({ date: reservation.date, barber: reservation.barber })
+      .toArray();
+    //STEP TWO: Check if the selected slot is available based on the reserved slots
+    const isSlotAvailable = reservations.every((res) => {
+      return !reservation.slot.some((slot) => res.slot.includes(slot));
+    });
+
+    if (!isSlotAvailable) {
+      return res.status(400).json({
+        status: 400,
+        message:
+          "Selected slot is already reserved. Please select another slot.",
+      });
+    }
+    //STEP THREE: Check if the selected slot will overlap with any of the reserved slots
+    const todayReservationStartingSlots = reservations.map(
+      (res) => res.slot[0]
+    );
+
+    const slotsToRemove = removeSlotsForOverLapping(
+      reservation.service.duration,
+      todayReservationStartingSlots
+    );
+    const isSlotOverlapping = slotsToRemove.every((slot) => {
+      return !reservation.slot.includes(slot);
+    });
+    if (!isSlotOverlapping) {
+      return res.status(400).json({
+        status: 400,
+        message: "Someone has just reserved that slot!",
+      });
+    }
     // check if client exists
     const isClient = await db
       .collection("Clients")
@@ -249,7 +363,7 @@ const addReservation = async (req, res) => {
           to: userInfo.number,
         });
       } catch (err) {
-        console.log(err);
+        console.error("Error sending SMS to user:", err);
       }
     }
 
