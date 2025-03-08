4ea2ed8dc66b02bdd5afe523d2f382030f38cc72
Roy Andraos <roy_andraos@live.fr>
2025-03-08 11:52:48 -0500

    message date fix

--- server/server.js
+++ server/server.js
@@ -310,19 +310,23 @@ const addReservation = async (req, res) => {
           body: `No Reply ~Hollywood Barbershop
             Bonjour ${reservation.fname} ${
             reservation.lname || ""
-          }, votre réservation au Hollywood Barbershop est confirmée pour aujourd'hui à ${
-            reservation.slot[0].split("-")[1]
-          } avec ${reservation.barber}. Vous recevrez une ${
-            reservation.service.name
-          } pour ${reservation.service.price} CAD. ~Hollywood Barbershop
+          }, votre réservation au Hollywood Barbershop est confirmée pour ${
+            reservation.date
+          } à ${reservation.slot[0].split("-")[1]} avec ${
+            reservation.barber
+          }. Vous recevrez une ${reservation.service.name} pour ${
+            reservation.service.price
+          } CAD. ~Hollywood Barbershop
     
             Hello ${reservation.fname} ${
             reservation.lname || ""
-          }, your reservation at Hollywood Barbershop is confirmed for today at ${
-            reservation.slot[0].split("-")[1]
-          } with ${reservation.barber}. You will be getting a ${
-            reservation.service.english
-          } for ${reservation.service.price} CAD. 
+          }, your reservation at Hollywood Barbershop is confirmed for ${
+            reservation.date
+          } at ${reservation.slot[0].split("-")[1]} with ${
+            reservation.barber
+          }. You will be getting a ${reservation.service.english} for ${
+            reservation.service.price
+          } CAD. 
               Pour annuler (to cancel): https://hollywoodfairmountbarbers.com/cancel/${
                 reservation._id
               }
